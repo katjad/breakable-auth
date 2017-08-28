2760965c4ccb99565757881e61cecbcc3b2c8589
Katja Durrani <mail@kdurrani.co.uk>
2017-08-28 12:49:49 +0100

    separate out authentication

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var auth = require('./auth');
 var passport = require('passport')
 var GitHubStrategy = require('passport-github').Strategy;
-var session = require('express-session')
+var session = require('express-session');
+var options = require('./appConfig');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -26,39 +28,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:'tswift', resave: true, saveUninitialized: true}))
 
-//Initialize passport and restore authentication state if available
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new GitHubStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: '/auth/github/callback',
-  },
-  function(accessToken, refreshToken, user, cb) {
-      return cb(null, user);
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
+// authentication
+auth(app);
 
 app.get('/', index)
-
-//OAuth authentication route
-app.get('/auth/github', passport.authenticate('github'));
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/' }), 
-  function(req, res) {
-  res.redirect('/users');
-});
 
 app.get('/users', ensureAuthenticated, users)
 
---
+++ auth/index.js
@@ -0,0 +1,39 @@
+// learned this pattern from Stephen Grider Udemy Tut 
+// on Authentication with React
+
+auth = function(app){    
+
+    app.use(session({secret:'tswift', resave: true, saveUninitialized: true}))
+
+    //Initialize passport and restore authentication state if available
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.use(new GitHubStrategy({
+        clientID: options.GITHUB_CLIENT_ID || process.env.CLIENT_ID,
+        clientSecret: options.GITHUB_CLIENT_SECRET || process.env.CLIENT_SECRET,
+        callbackURL: '/auth/github/callback',
+      },
+      function(accessToken, refreshToken, user, cb) {
+          return cb(null, user);
+      }
+    ));
+
+    passport.serializeUser(function(user, done) {
+      done(null, user);
+    });
+
+    passport.deserializeUser(function(obj, done) {
+      done(null, obj);
+    });
+
+    //OAuth authentication route
+    app.get('/auth/github', passport.authenticate('github'));
+    app.get('/auth/github/callback', 
+      passport.authenticate('github', { failureRedirect: '/' }), 
+      function(req, res) {
+      res.redirect('/users');
+    })
+;}
+
+module.exports = auth
